26e133049bde5a6a9524671aa363c2b052e29c9a
spica1108 <815303067@qq.com>
2025-12-01 22:43:21 +0800

    完成数据实时更新

--- src/App.vue
+++ src/App.vue
@@ -2,46 +2,11 @@
 
 import { ref } from 'vue';
 import { Icon } from '@iconify/vue';
-import { ConversationProps, ProviderProps } from './types';
 import ConversationList from './components/ConversationList.vue';
 import ProviderSelect from './components/ProviderSelect.vue';
 import MessageInput from './components/MessageInput.vue';
 const selectedModel = ref();
-
-const items: ConversationProps[] = [
-  { id: 1, selectedModel: 'gpt-3.5-turbo', title: 'Conversation 1', createdAt: '2024-06-01 10:00',updatedAt: '2024-06-01 10:00' },
-  { id: 2, selectedModel: 'gpt-4', title: 'Conversation 2', createdAt: '2024-06-02 11:30',updatedAt: '2024-06-02 11:30' },
-  { id: 3, selectedModel: 'gpt-3.5-turbo', title: 'Conversation 3', createdAt: '2024-06-03 14:15', updatedAt: '2024-06-03 14:15' },
-  { id: 1, selectedModel: 'gpt-3.5-turbo', title: 'Conversation 1', createdAt: '2024-06-01 10:00',updatedAt: '2024-06-01 10:00' },
-  { id: 2, selectedModel: 'gpt-4', title: 'Conversation 2', createdAt: '2024-06-02 11:30',updatedAt: '2024-06-02 11:30' },
-  { id: 3, selectedModel: 'gpt-3.5-turbo', title: 'Conversation 3', createdAt: '2024-06-03 14:15', updatedAt: '2024-06-03 14:15' },
-  { id: 1, selectedModel: 'gpt-3.5-turbo', title: 'Conversation 1', createdAt: '2024-06-01 10:00',updatedAt: '2024-06-01 10:00' },
-  { id: 2, selectedModel: 'gpt-4', title: 'Conversation 2', createdAt: '2024-06-02 11:30',updatedAt: '2024-06-02 11:30' },
-  { id: 3, selectedModel: 'gpt-3.5-turbo', title: 'Conversation 3', createdAt: '2024-06-03 14:15', updatedAt: '2024-06-03 14:15' },
-];
-
-const providers: ProviderProps[] = [
-  {
-  id: 1,
-  name: '文心一言',
-  desc: '文心一言 百度出品的大模型',
-  models: ['ERNIE-4.0-8K', 'ERNIE-3.5-8K', 'ERNIE-Speed-8K'], 
-  avatar: 'https://img.alicdn.com/imgextra/i4/O1CN01RyvC8Z1sUKjO6NtNl_!!6000000005765-2-tps-200-200.png',
-  createdAt: '2024-07-03',
-  updatedAt: '2024-07-03'
-  },
-  {
-    id: 2,
-    name: '通义千问',
-    desc: '通义千问',
-    // https://help.aliyun.com/zh/dashscope/developer-reference/api-details?spm=a2c4g.11186623.0.
-    models: ['qwen-turbo', 'qwen-plus', 'qwen-max'],
-    avatar: 'https://img.alicdn.com/imgextra/i4/O1CN01RyvC8Z1sUKjO6NtNl_!!6000000005765-2-tps-200-200.png',
-    createdAt: '2024-07-03',
-    updatedAt: '2024-07-03'
-  }
-]
-
+import { conversations } from './testData'
 console.log('This message is being logged by "App.vue", included via Vite');
 
 </script>
@@ -55,7 +20,7 @@ console.log('This message is being logged by "App.vue", included via Vite');
       <RouterLink to="/conversation">
       <!-- 底部按钮固定，超过多少会向下滑动 -->
       <div class="h-[90%] overflow-y-auto">
-        <ConversationList :items="items" />
+        <ConversationList :items="conversations" />
       </div>
       </RouterLink>
 
--- src/views/Conversation.vue
+++ src/views/Conversation.vue
@@ -1,41 +1,43 @@
 <script setup lang="ts">
-import { MessageProps } from '../types';
+import { ref,watch  } from 'vue';
 import MessageInput from '../components/MessageInput.vue';
 import MessageList from '../components/MessageList.vue';
 import { useRoute } from 'vue-router';
+import { MessageProps,ConversationProps } from '../types';
+import { messages,conversations } from '../testData';
 
 const route = useRoute();
+const filteredMessages = ref<MessageProps[]>([]);//类型，改成响应式数据
+const conversation = ref<ConversationProps>();
 // 获取路由参数中的 conversationId
 //转换成number类型
-//as string：TypeScript 的类型断言，告诉编译器“我知道它是字符串”
-const conversationId = parseInt(route.params.id as string);
+//as string：TypeScript 的类型断言，告诉编译器“我知道,它是字符串”
+ let conversationId = parseInt(route.params.id as string);
 //route 来自 useRoute()（vue-router），route.params 存放路径参数（动态路由片段）
 //route.params.id 对应路由定义中的占位符，例如路由 '/conversation/:id'，
 //当 URL 是 '/conversation/2' 时，route.params.id === '2'
 
-const messages: MessageProps[] = [
-  { id: 1, content: '什么是光合作用', createdAt: '2024-07-03', updatedAt: '2024-07-03', type: 'question', conversationId: 1 },
-  { id: 2, content: '你的说法很请正确，理解的很不错,你的说法很请正确，理解的很不错', createdAt: '2024-07-03', updatedAt: '2024-07-03', type: 'answer', conversationId: 1 },
-  { id: 3, content: '请告诉我更多', createdAt: '2024-07-03',updatedAt: '2024-07-03',  type: 'question', conversationId: 1 },
-  { id: 4, content: '你的说法很请正确，理解的很不错,你的说法很请正确，理解的很不错', createdAt: '2024-07-03', updatedAt: '2024-07-03', type: 'answer', conversationId: 1 },
-  { id: 5, content: '还有更多的信息吗', createdAt: '2024-07-03', type: 'question', updatedAt: '2024-07-03', conversationId: 1 },
-  { id: 6, content: '', createdAt: '2024-07-03', updatedAt: '2024-07-03', type: 'answer', status: 'loading', conversationId: 1 },
-  { id: 7, content: '什么是光合作用', createdAt: '2024-07-03', updatedAt: '2024-07-03', type: 'question', conversationId: 2 },
-  { id: 8, content: '你的说法很请正确，理解的很不错,你的说法很请正确，理解的很不错', createdAt: '2024-07-03', updatedAt: '2024-07-03', type: 'answer', conversationId: 2 },
-  { id: 9, content: '请告诉我更多', createdAt: '2024-07-03',updatedAt: '2024-07-03',  type: 'question', conversationId: 2 },
-  { id: 10, content: '你的说法很请正确，理解的很不错,你的说法很请正确，理解的很不错', createdAt: '2024-07-03', updatedAt: '2024-07-03', type: 'answer', conversationId: 2 },
-  { id: 11, content: '还有更多的信息吗', createdAt: '2024-07-03', type: 'question', updatedAt: '2024-07-03', conversationId: 3 },
-  { id: 12, content: '', createdAt: '2024-07-03', updatedAt: '2024-07-03', type: 'answer', status: 'loading', conversationId: 3 },
-];
 //把 messages 中所有 conversationId 等于当前 conversationId 的消息挑出来，
 // 赋值给 filteredMessages，通常用于只显示当前会话的消息列表
-const filteredMessages = messages.filter(message => message.conversationId === conversationId);
+filteredMessages.value = messages.filter(message => message.conversationId === conversationId);
+//空值检查，强制检查可能为none和undefined的值，避免报错
+conversation.value = conversations.find(item => item.id === conversationId)
+//route是响应式的，但是route.params.id不是响应式的，使用watch监听route.params.id的变化
+watch(() => route.params.id, (newId: string) => {
+  conversationId = parseInt(newId);
+  filteredMessages.value = messages.filter(message => message.conversationId === conversationId);
+  conversation.value = conversations.find(item => item.id === conversationId)
+});
 </script>
 
-
 <template>
+  <!-- 会话标题栏 -->
+  <div class="h-[5%] bg-gray-200 border-b border-gray-300 flex items-center px-3 justify-between" v-if="conversation">
+    <h3 class="font-semibold text-gray-900">{{ conversation.title }}</h3>
+    <span class="text-sm text-gray-500">{{ conversation.updatedAt }}</span>
+  </div>
 <!-- 会发生滚动效果 -->
-  <div class="w-[80%] mx-auto h-[85%] overflow-y-auto pt-2">
+  <div class="w-[80%] mx-auto h-[75%] overflow-y-auto pt-2">
     <MessageList :messages="filteredMessages" />
   </div>
   <div class="w-[80%] mx-auto h-[15%] flex items-center">
